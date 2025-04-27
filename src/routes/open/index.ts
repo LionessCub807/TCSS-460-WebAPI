9d0fcfef588e67d4b9df65094edbaa731891f047
Bindi <bindin42@gmail.com>
2025-04-26 21:09:06 -0700

    Search book by ISBN route

--- src/routes/open/index.ts
+++ src/routes/open/index.ts
@@ -1,9 +1,11 @@
 import express, { Router } from 'express';
 
 import { messageRouter } from './message';
+import { isbnRouter } from './isbn';
 
 const openRoutes: Router = express.Router();
 
 openRoutes.use('/message', messageRouter);
+openRoutes.use('/isbn', isbnRouter);
 
 export { openRoutes };
