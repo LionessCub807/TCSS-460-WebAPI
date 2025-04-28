6c459be47c359b43c8ed7b6c55afde36a8abc174
Selly Beyene <sbeyen@uw.edu>
2025-04-27 22:15:18 -0700

    Updated table schema in heroku database.

--- data/heroku.sql
+++ data/heroku.sql
@@ -26,20 +26,77 @@ CREATE TABLE Account_Credential (Credential_ID SERIAL PRIMARY KEY,
 );
 
 
-DROP TABLE IF EXISTS BOOKS;
-CREATE TABLE BOOKS (id INT PRIMARY KEY,
-        isbn13 BIGINT,
-        authors TEXT,
+--DROP TABLE IF EXISTS BOOKS;
+--CREATE TABLE BOOKS (id INT PRIMARY KEY,
+        --isbn13 BIGINT,
+        --authors TEXT,
+        --publication_year INT,
+        --original_title TEXT,
+        --title TEXT,
+        --rating_avg FLOAT,
+        --rating_count INT,
+        --rating_1_star INT,
+        --rating_2_star INT,
+        --rating_3_star INT,
+        --rating_4_star INT,
+        --rating_5_star INT,
+        --image_url TEXT,
+        --image_small_url TEXT
+    --);
+
+    -- Temporary table
+-- Only used to move data from CSV file to SQL tables
+DROP TABLE IF EXISTS tempbook CASCADE;
+CREATE TABLE tempbook (book_id INT,
+    isbn13 BIGINT,
+    authors TEXT,
+    original_publication_year INT,
+    original_title TEXT,
+    title TEXT,
+    average_rating FLOAT,
+    ratings_count INT,
+    ratings_1 INT,
+    ratings_2 INT,
+    ratings_3 INT,
+    ratings_4 INT,
+    ratings_5 INT,
+    image_url TEXT,
+    small_image_url TEXT
+);
+
+-- Book table
+DROP TABLE IF EXISTS Books CASCADE;
+CREATE TABLE Books (bookid SERIAL PRIMARY KEY,
+        isbn13 BIGINT UNIQUE,
         publication_year INT,
         original_title TEXT,
         title TEXT,
-        rating_avg FLOAT,
-        rating_count INT,
+        image_url TEXT,
+        image_small_url TEXT
+    );
+
+-- Author table
+DROP TABLE IF EXISTS Authors CASCADE;
+CREATE TABLE Authors(authorid SERIAL PRIMARY KEY,
+        authorname TEXT
+);
+
+-- Book and author transaction table
+DROP TABLE IF EXISTS BookAuthor CASCADE;
+CREATE TABLE BookAuthor (authorid INT,
+        bookid INT,
+        FOREIGN KEY (authorid) REFERENCES Authors(authorid),
+        FOREIGN KEY (bookid) REFERENCES Books(bookid)
+);
+
+-- Ratings Table
+DROP TABLE IF EXISTS Ratings CASCADE;
+CREATE TABLE Ratings(ratingid SERIAL PRIMARY KEY,
+        bookid INT,
         rating_1_star INT,
         rating_2_star INT,
         rating_3_star INT,
         rating_4_star INT,
         rating_5_star INT,
-        image_url TEXT,
-        image_small_url TEXT
-    );
+        FOREIGN KEY (bookid) REFERENCES Books(bookid)
+);
