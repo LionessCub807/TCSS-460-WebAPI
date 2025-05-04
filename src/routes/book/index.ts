880389615b10be28405c87d514095e72e997dea8
Selly Beyene <sbeyen@uw.edu>
2025-05-03 21:40:43 -0700

    Completed updating a rating route.

--- src/routes/book/index.ts
+++ src/routes/book/index.ts
@@ -2,8 +2,12 @@ import express, { Router } from 'express';
 
 import { createRouter } from './createbook';
 
+import { updateRatingRouter } from './updaterating';
+
 const bookRoutes: Router = express.Router();
 
 bookRoutes.use('/books/new', createRouter);
+
+bookRoutes.use('/books', updateRatingRouter);
 
 export { bookRoutes };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import express, { Router } from 'express';
 import { openRoutes } from './open';
 import { authRoutes } from './auth';
 import { closedRoutes } from './closed';
+import { bookRoutes } from './book';
 
 const routes: Router = express.Router();
 
 routes.use(openRoutes);
 routes.use(authRoutes);
 routes.use(closedRoutes);
+routes.use(bookRoutes);
 
 export { routes };
