87a910280ed0c1f7c9f5230a32cfb60a8d5587aa
Selly Beyene <sbeyen@uw.edu>
2025-05-10 15:24:43 -0700

    Added delete a book by ISBN route, and updated inti.sql to add cascading deletes for BookAuthor table and Ratings table.

--- src/routes/book/index.ts
+++ src/routes/book/index.ts
@@ -6,6 +6,8 @@ import { updateRatingRouter } from './updaterating';
 
 import { retrieveAllRouter } from './retrievebooks';
 
+import { deleteBookByISBNRouter } from './deleteBookByISBN';
+
 const bookRoutes: Router = express.Router();
 
 bookRoutes.use('/books/new', createRouter);
@@ -13,5 +15,7 @@ bookRoutes.use('/books/new', createRouter);
 bookRoutes.use('/books', updateRatingRouter);
 
 bookRoutes.use('/books/all', retrieveAllRouter);
+
+bookRoutes.use('/books', deleteBookByISBNRouter);
 
 export { bookRoutes };
